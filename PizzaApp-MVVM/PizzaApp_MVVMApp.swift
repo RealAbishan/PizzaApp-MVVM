a5a4d60f8bac8b4219f6d98cc780be77744fe54c
Abishan <highoncode09@gmail.com>
2023-03-09 12:38:41 +0530

    Initial Commit

---
+++ PizzaApp-MVVM/PizzaApp_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaApp_MVVMApp.swift
+//  PizzaApp-MVVM
+//
+//  Created by Abishan Parameswaran on 2023-03-09.
+//
+
+import SwiftUI
+
+@main
+struct PizzaApp_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
